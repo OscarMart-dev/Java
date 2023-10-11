3885ff65fcff41161de42532c63cd1907c33655e
OscarMart-dev <oskmart23@gmail.com>
2023-10-11 11:28:29 -0500

    :v

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,5 @@
+public class HolaMundo {
+    public static void main (String [] args){
+        System.out.print("Hola mi nombre es oscar Martinez");
+    }
+}
---
+++ HolaMundo/src/MiNombre.java
@@ -0,0 +1,8 @@
+public class MiNombre {
+    public static void main(String[] args) {
+        String nombre="Oscar";
+        String apellido="Martinez";
+        System.out.print("Hola, mi nombre es "+nombre+" " +apellido);
+
+    }
+}
